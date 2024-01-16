1cd6c6ed9299d2b969a504caaafab6a00d528441
dhruv <dhruvjn110327>
2024-01-17 00:51:59 +0530

    variables, alert, if else, console.log

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,9 +1,12 @@
-/*
-////////////////////////////////////
-// Linking a JavaScript File
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
+if (js == "a") alert("js");
+
+40 + 8 + 0;
+/*
+////////////////////////////////////
+// Linking a JavaScript File
 ////////////////////////////////////
 // Values and Variables
 console.log("Jonas");
